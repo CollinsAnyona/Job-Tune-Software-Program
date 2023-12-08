5e83a15b31b4ff61327b6d3c61d6c5838145e7c5
lmurayire12 <l.murayire@alustudent.com>
2023-12-08 02:08:34 -0800

    job tune

--- jobtune.py
+++ jobtune.py
@@ -3,6 +3,72 @@ import sqlite3
 from prettytable import PrettyTable
 from companies_data import companies
 
+def check_profile():
+    existing_profile = input("Are you a new user? (yes/no): ").lower()
+
+    if existing_profile == "yes":
+        create_profile()
+    elif existing_profile == "no":
+        continue_profile()
+    else:
+        print("Invalid input. Please enter 'yes' or 'no'.")
+        check_profile()
+
+def continue_profile():
+    print("Welcome back! Let's continue with your profile.")
+
+def create_profile():
+    print("Let's create a new profile for you")
+    print(" ")
+    # Get name of the user and reject if the name is only one
+    user_name = input("Please enter your full names: ")
+    names = user_name.split()
+
+    if len(names) < 2:
+        print("You have entered only one name, kindly input at least two names")
+        user_name = input("Please enter your full names again: ")
+
+    print(" ")
+    print(f"Thank you {user_name} for choosing us. May you continue with the signing in")
+
+    # Display available industries and prompt the user to choose
+    def display_industries():
+        industries_list = [
+            {"id": 1, "name": "Agricultural Industry"},
+            {"id": 17, "name": "Technology Industry"},
+            {"id": 16, "name": "Automobile and Accessories"},
+            {"id": 4, "name": "Financial Industry"},
+            {"id": 15, "name": "Commercial and Professional Services"},
+            {"id": 13, "name": "E-commerce and Retail Industry"},
+            {"id": 10, "name": "Energy and Environment Resources"},
+            {"id": 9, "name": "Travel and Leisure Industry"},
+            {"id": 8, "name": "Construction and Real Estate"},
+            {"id": 7, "name": "Health and Beauty"},
+            {"id": 6, "name": "Hotels, Lodges, and Tourism Industry"},
+            {"id": 5, "name": "Services Industry"},
+            {"id": 4, "name": "Home and Office Furniture"},
+            {"id": 3, "name": "Foodstuff and Beverages"},
+            {"id": 2, "name": "Electricals and Electronics"},
+        ]
+
+        print("The following are the available Industries:")
+        print(" ")
+        for industry in industries_list:
+            print(f"ID: {industry['id']}, Name: {industry['name']}")
+
+        while True:
+            try:
+                user_industry_id = int(input("Enter the ID of the industry you are interested in: "))
+                if any(industry['id'] == user_industry_id for industry in industries_list):
+                    break
+                else:
+                    print("Invalid ID. Please enter a valid ID from the list.")
+            except ValueError:
+                print("Invalid input. Please enter a valid numeric ID.")
+
+        return user_industry_id
+    user_industry_id = display_industries()
+
 # Get name of the user and reject if the name is only one
 user_name = input("Welcome. Please enter your full names: ")
 names = user_name.split()
